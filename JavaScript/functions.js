955fc25219eb0057a8bcfa64fc1de99815264324
= <=>
2024-02-12 11:28:36 -0600

    estilo de formulario

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -2,3 +2,13 @@ function changeColor(){
 	document.getElementById("xd").style.backgroundColor="black";
         document.getElementById("xd").style.color="white";
 }
+function sendMessage(){
+alert("Saludo!");
+}
+<script type="text/javascript">
+//auto expand textarea
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
+</script>
